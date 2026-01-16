b1de13df156eae20fc32b574db48b8764880c112
zaynabali-12345 <zaynabali00786@gmail.com>
2026-01-16 16:59:29 +0530

    Delete lest_models_hr.py

--- lest_models_hr.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyDT5zcoMOgAa7E8_haS-jzpEOH5-9NDrlY")
-
-for m in genai.list_models():
-    if "generateContent" in m.supported_generation_methods:
-        print(m.name)
